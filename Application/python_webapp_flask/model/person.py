0fb23a2f61b9241c54cb40839014cbdf2ec59e53
Kushal <kushaldeveloper@gmail.com>
2019-02-08 16:04:16 -0500

    add more details to person

--- Application/python_webapp_flask/model/person.py
+++ Application/python_webapp_flask/model/person.py
@@ -1,8 +1,18 @@
 import uuid
+from enum import Enum, auto
 
 import attr
+
+
+class LoginType(Enum):
+    PASSWORD = auto()
+    LDAP = auto()
 
 
 @attr.s(auto_attribs=True)
 class Person(object):
     id: uuid.uuid4()
+    name: str
+    email: str
+    password: str
+    login_type: LoginType.name
